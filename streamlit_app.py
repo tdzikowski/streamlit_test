906c012e1fc6c37a6da43bf314322421c57b1cd6
tdzikowski <120711733+tdzikowski@users.noreply.github.com>
2024-09-20 07:35:17 +0000

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,34 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 
 st.title('Hello World')
 st.write('**Data Science Course 2024 TD**')
 
-st.write('testy test')
 
 df = pd.read_csv('data/Bastar Craton.csv')
-st.dataframe(df)
+#st.dataframe(df)
+
+cat_names = df.columns.tolist()[27:]
+col1, col2 = st.columns()
+
+el1 = st.selectbox('x-axis', cat_names)
+el2 = st.selectbox('y-axis', cat_names)
+
+with col1:
+    el1 = st.selectbox('x-axis', cat_names)
+    st.write(el1)
+
+with col2:
+    el2 = st.selectbox('y-axis', cat_names)
+    st.write(el2)
+
+
+
+
+
+
 
 
 
