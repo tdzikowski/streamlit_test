644e259f9acea14b0ca03c8bd219f6beff92f91f
tdzikowski <120711733+tdzikowski@users.noreply.github.com>
2024-09-20 07:21:58 +0000

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,20 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 st.title('Hello World')
 st.write('**Data Science Course 2024 TD**')
 
 st.write('testy test')
 
-#dsfjsdlökj
+df = pd.read_csv('data/Bastar Craton.csv')
+st.dataframe(df)
+
+
+
+#plt.scatter(df[0],df[1])
+#plt.show()
 
 
 #jjhg
